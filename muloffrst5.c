dab15455f399bdf64d8e6767e92f5664ce187ce2
Mohammedsiddiq05 <31438748+Mohammedsiddiq05@users.noreply.github.com>
2018-02-12 16:58:40 +0530

    Create muloffrst5.c

---
+++ muloffrst5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i,n,result;
+printf("\nenter the num");
+scanf("%d",&n);
+for(i=1;i<6;i++)
+{
+result=n*i;
+printf("\nmul of frst five is:%d",result);
+}
+}
+
+
